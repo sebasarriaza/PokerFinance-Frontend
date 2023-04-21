f85f415f683ff9f287c024f3867b8f2452262a09
sebas <sebas28ac@gmail.com>
2023-04-21 18:23:30 +0200

    Styling cards

--- src/components/CreateOffer.tsx
+++ src/components/CreateOffer.tsx
@@ -36,61 +36,50 @@ function CreateOffer() {
 
   return (
     <div className={styles.container}>
-    <div className="mt-5 ml-5 mr-5 mb-5 bg-gray-900 p-2 rounded-sm border-2 border-gray-100  sm:w-[70%] lg:max-w-[30%]">
-      <h2 className="text-center pb-2 text-2xl">Poker Factory</h2>
+      <h2>Poker Factory</h2>
 
       <div className="flex items-center justify-between p-1 mt-2">
-        <div className="flex items-center justify-center">
-          <p>Here you can create new offers:</p>
-        </div>
+        <div className={styles['input-label']}>Here you can create new offers:</div>
       </div>
 
       <div className="flex items-center justify-between p-1 mt-2">
-        <p>Amount to finance:</p>
+        <div className={styles['input-label']}>Amount to finance:</div>
         <div className="flex items-center justify-center">
           <input
             type="number"
             value={factoryAmountToFinance}
             onChange={getFactoryAmountToFinance}
-            style={{ color: "black" }}
           />
         </div>
       </div>
 
       <div className="flex items-center justify-between p-1 mt-2">
-        <p>Player fee (must be greater than 15):</p>
+        <div className={styles['input-label']}>Player fee (must be greater than 15):</div>
         <div className="flex items-center justify-center">
           <input
             type="number"
             value={factoryPlayerFee}
             onChange={getFactoryPlayerFee}
-            style={{ color: "black" }}
           />
         </div>
       </div>
 
       <div className="flex items-center justify-between p-1 mt-2">
-        <p>DatetimeLimit:</p>
+        <div className={styles['input-label']}>DatetimeLimit:</div>
         <div className="flex items-center justify-center">
           <input
             type="number"
             value={factoryDatetimeLimit}
             onChange={getFactoryDatetimeLimit}
-            style={{ color: "black" }}
           />
         </div>
       </div>
 
       <div className="flex items-center justify-center">
-        <button
-          className="text-2xl bg-red-500 pl-3 pr-3 rounded-sm"
-          onClick={createOffer}
-        >
-          {" "}
-          Create Offer{" "}
+        <button className={styles.button} onClick={createOffer}>
+          Create Offer
         </button>
       </div>
-    </div>
     </div>
   );
 }
