a4a88fde619ae991c993d85c08d6edd524fb6675
sebas <sebas28ac@gmail.com>
2023-04-24 22:25:45 +0200

    Fix error className

--- src/components/FinanceCard.tsx
+++ src/components/FinanceCard.tsx
@@ -11,108 +11,93 @@ import { useContractWrite } from "wagmi";
 import { usePrepareContractWrite } from "wagmi";
 
 export default function FinanceCard() {
-  const provider = useProvider();
 
+  
+  const provider = useProvider()
+  
   const [addressContract, setAddressContract] = useState<string[]>();
   const [addressPlayer, setAddressPlayer] = useState<string[]>();
   const [amountTotal, setAmountTotal] = useState<number[]>();
   const [fee, setFee] = useState<number[]>();
   const [datetimeLimit, setDatetimeLimit] = useState<number[]>();
   const [viewButton, setViewButton] = useState<boolean>(false);
-  const addressPlayerArray: string[] = [];
-  const amountTotalArray: number[] = [];
-  const feeArray: number[] = [];
-  const datetimeLimitArray: number[] = [];
-  let selectedContract: string = "";
+  const addressPlayerArray:string[] = [];
+  const amountTotalArray:number[] = [];
+  const feeArray:number[] = [];
+  const datetimeLimitArray:number[] = [];
+  let selectedContract:string = "";
 
   function getDate(datestamp: number) {
-    let dateFormat = new Date(datestamp * 1000);
 
-    return (
-      dateFormat.getDate() +
-      "/" +
-      (dateFormat.getMonth() + 1) +
-      "/" +
-      dateFormat.getFullYear() +
-      " " +
-      ("0" + dateFormat.getHours()).slice(-2) +
-      ":" +
-      ("0" + dateFormat.getMinutes()).slice(-2) +
-      ":" +
-      ("0" + dateFormat.getSeconds()).slice(-2)
-    );
-  }
+    let dateFormat= new Date(datestamp*1000);
 
-  useEffect(() => {
-    async function connectPokerFactoryContract() {
-      const connectedPokerFactoryContract = new ethers.Contract(
+    return dateFormat.getDate()+
+       "/"+(dateFormat.getMonth()+1)+ 
+       "/"+dateFormat.getFullYear()+
+       " "+("0" + dateFormat.getHours()).slice(-2)+
+       ":"+("0" + dateFormat.getMinutes()).slice(-2)+
+       ":"+("0" + dateFormat.getSeconds()).slice(-2); 
+}
+
+  useEffect (()=>{
+
+    async function connectPokerFactoryContract(){
+      const connectedPokerFactoryContract =  new ethers.Contract(
         pokerFactoryContract.address,
         pokerFactoryContract.abi,
         provider
-      );
-      const pokerFactoryContractEvents =
-        await connectedPokerFactoryContract.queryFilter("PokerFinanceCreated");
-      console.log(pokerFactoryContractEvents, "past events");
-      for (let i = 0; i < pokerFactoryContractEvents.length; i++) {
-        console.log(pokerFactoryContractEvents[i].args);
-        addressPlayerArray.push(
-          pokerFactoryContractEvents && pokerFactoryContractEvents[i].args
-            ? pokerFactoryContractEvents[i].args[0]
-            : ""
         );
-        amountTotalArray.push(
-          pokerFactoryContractEvents && pokerFactoryContractEvents[i].args
-            ? pokerFactoryContractEvents[i].args[2]
-            : 0
+        const pokerFactoryContractEvents = await connectedPokerFactoryContract.queryFilter(
+        "PokerFinanceCreated"
         );
-        feeArray.push(
-          pokerFactoryContractEvents && pokerFactoryContractEvents[i].args
-            ? pokerFactoryContractEvents[i].args[3]
-            : 0
-        );
-        datetimeLimitArray.push(
-          pokerFactoryContractEvents && pokerFactoryContractEvents[i].args
-            ? Number(pokerFactoryContractEvents[i].args[4])
-            : 0
-        );
-      }
-      setAddressPlayer(addressPlayerArray);
-      setAmountTotal(amountTotalArray);
-      setFee(feeArray);
-      setDatetimeLimit(datetimeLimitArray);
-      setAddressPlayer(addressPlayerArray);
+        console.log(pokerFactoryContractEvents, "past events");
+        for (let i = 0; i < pokerFactoryContractEvents.length; i++) {
+          console.log(pokerFactoryContractEvents[i].args)
+          addressPlayerArray.push(pokerFactoryContractEvents && pokerFactoryContractEvents[i].args ? pokerFactoryContractEvents[i].args[0] : '')
+          amountTotalArray.push(pokerFactoryContractEvents && pokerFactoryContractEvents[i].args ? pokerFactoryContractEvents[i].args[2] : 0)
+          feeArray.push(pokerFactoryContractEvents && pokerFactoryContractEvents[i].args ? pokerFactoryContractEvents[i].args[3] : 0)
+          datetimeLimitArray.push(pokerFactoryContractEvents && pokerFactoryContractEvents[i].args ? Number(pokerFactoryContractEvents[i].args[4]) : 0)
+        }
+        setAddressPlayer(addressPlayerArray);
+        setAmountTotal(amountTotalArray);
+        setFee(feeArray);
+        setDatetimeLimit(datetimeLimitArray);
+        setAddressPlayer(addressPlayerArray);
     }
-    connectPokerFactoryContract();
-  }, []);
-
+    connectPokerFactoryContract()
+  
+  }, [])
+  
   const [amount, setAmount] = useState<number>(0);
 
-  const handleInputChange = (event: any) => {
+  const handleInputChange = (event:any) => {
     setAmount(event.target.value);
   };
 
-  const { config, error } = usePrepareContractWrite({
+  const { config, error} = usePrepareContractWrite({
     address: "0x72f4fea1c8224f4baa6f3cced273b13e1c5cb21c",
     abi: ABI,
     functionName: "finance",
     args: [amount],
   });
-
+  
   const { write, status } = useContractWrite(config);
-
-  console.log(status);
-  function callFinance(index: number) {
+  
+  console.log(status)
+  function callFinance(index:number){
     if (addressPlayer == undefined) return;
     selectedContract = addressPlayer[index];
-    write?.();
-    console.log(selectedContract, "selectedContract");
-    console.log(error);
+    write?.()
+    console.log(selectedContract, "selectedContract")
+    console.log(error)
   }
+
 
   return (
     <>
-      {addressPlayer?.map((item, index) => (
-        <div className={styles.container} key={index}>
+    {addressPlayer?.map((item, index) => (
+    
+    <div className={styles.container} key={index}>
           <div className={styles.card}>
             <p className={styles.index}>{index}</p>
             <p className={styles.addressPlayer}>
@@ -153,7 +138,7 @@ export default function FinanceCard() {
             </div>
           </div>
         </div>
-      ))}
+    ))}
     </>
   );
 }
